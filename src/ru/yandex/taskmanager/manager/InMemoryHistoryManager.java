b79c4561baafd4f3f4cbf6ec54ae170909455d26
KuznetsovME <MakkowniS@yandex.ru>
2025-09-15 14:32:30 +1000

    Изменить реализацию истории в виде hashmap и двусвязного списка с новым классом Node реализовать возможность удаления задач из истории + добавить автоматическое удаление записей при удалении основных задач.

--- src/ru/yandex/taskmanager/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/taskmanager/manager/InMemoryHistoryManager.java
@@ -3,25 +3,72 @@ package ru.yandex.taskmanager.manager;
 import ru.yandex.taskmanager.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final ArrayList<Task> history = new ArrayList<>(); // Список для хранения истории
-    static final int HISTORY_LIMIT = 10;
+    Map<Integer, Node<Task>> historyMap = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+
+    private void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+    }
+
+    private void removeNode(Node<Task> node) {
+        Node<Task> prev = node.prev;
+        Node<Task> next = node.next;
+
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+        }
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+        }
+    }
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            history.add(task.objectCopy());
-            if (history.size() > HISTORY_LIMIT) {
-                history.removeFirst();
-            }
+        if (task == null) return;
+
+        remove(task.getId()); // Удаление предыдущего просмотра задачи, если есть
+
+        Task taskToAdd = task.objectCopy(); // Копирование задачи для добавления
+        linkLast(taskToAdd); // Добавление в конец списка
+        historyMap.put(taskToAdd.getId(), tail);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = historyMap.remove(id);
+        if (node != null) {
+            removeNode(node);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        List<Task> history = new ArrayList<>();
+        Node<Task> node = head;
+
+        while (node != null) {
+            history.add(node.data);
+            node = node.next;
+        }
+
+        return history;
     }
 }
